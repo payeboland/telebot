d3db54d51e37e4b0c43f6ef1b1c457d81b3bbdb4
payeboland <payeboland2014@gmail.com>
2016-11-15 19:13:10 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 import webapp2
 
-TOKEN = 'YOUR_BOT_TOKEN_HERE'
+TOKEN = '200657939:AAEvM5T3WghxDBZRQ2tM680abBUFmAseUxc'
 
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 
